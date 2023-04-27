121d670fe215265446476cb1a69c449309de97fd
Andrew Woodring <woodring.andrew@yahoo.com>
2023-04-27 13:50:11 -0400

    second commit

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// this is for the graph
+// 1.) make "food" variable all preset to 0
+// 2.) when money is put into variable turn it into a percent.
+// 3.) after its turned into a percent, store that percent in a variable.
+// 4.) use percent variable to adjust the height of whichever div.
