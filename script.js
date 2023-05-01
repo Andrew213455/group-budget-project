f19849a91d62bd14e17f3255648046e93449ce09
Andrew Woodring <woodring.andrew@yahoo.com>
2023-05-01 10:13:42 -0400

    some changes

--- script.js
+++ script.js
@@ -51,25 +51,27 @@ let bills = 0;
 let category = "";
 
 const condescendingMessagefunction = () => {
-  let totalSpentPercent = totalSpent/+initialMoney.value * 100;
+  let totalSpentPercent = (totalSpent / +initialMoney.value) * 100;
   if (totalSpentPercent > 100) {
-    dynamicMessage.innerText = "your in debt";
-    } else if (totalSpentPercent >= 80) {
-    dynamicMessage.innerText = "you still have some money left";
-    } else if (totalSpentPercent >= 60) {
-    dynamicMessage.innerText = "you spent less than 80% of your money";
-    } else if (totalSpentPercent >= 40) {
-    dynamicMessage.innerText = "you spent less than 60% of your money";
-    } else if (totalSpentPercent >= 20) {
-    dynamicMessage.innerText = "you spent less than 40% of your money";
-    } else if (totalSpentPercent > 0) {
-    dynamicMessage.innerText = "you spent less than 20% of your money";
-    } else if (totalSpentPercent < 0) {
-    dynamicMessage.innerText = "you made extra money?? would you mind donating?"
-    } else {
-    dynamicMessage.innerText = ` ${name.value}, you still have some money left`;
-    }
-}
+    dynamicMessage.innerText = `Hey ${name.value}, where are you getting this imaginary money from...? We don't take Monopoly money here.`;
+  } else if (totalSpentPercent === 100) {
+    dynamicMessage.innerText = `Hey ${name.value}, STOP SPENDING! You're all out of money...`;
+  } else if (totalSpentPercent >= 80) {
+    dynamicMessage.innerText = `Woah there ${name.value}! You're not too good at this budgeting thing...`;
+  } else if (totalSpentPercent >= 60) {
+    dynamicMessage.innerText = `Actually, maybe you should ease up a bit, you only have about 40% of your budget left`;
+  } else if (totalSpentPercent >= 40) {
+    dynamicMessage.innerText = `Woaaahhh you're halfway there! WOOOAAAH Spend without a care!`;
+  } else if (totalSpentPercent >= 20) {
+    dynamicMessage.innerText = `Still looking good! So responsible! Go get yourself some concert tickets or something.`;
+  } else if (totalSpentPercent > 0) {
+    dynamicMessage.innerText = `Nice job ${name.value}, you still have a ton of money to go treat yourself`;
+  } else if (totalSpentPercent < 0) {
+    dynamicMessage.innerText = `I'm proud of you ${name.value}, you somehow have more money than you started with!`;
+  } else {
+    dynamicMessage.innerText = ` ${name.value}, welcome to the new month! Full money = full send!`;
+  }
+};
 
 popUpBox.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -193,15 +195,26 @@ expensesForm.addEventListener("submit", (e) => {
   totalBar.style.height = `${(runningTotal /10) * 4}px`;
   totalBar.style.backgroundColor = "grey";
   totalBar.style.border = "gray";
-
+  console.log(runningTotal);
   condescendingMessagefunction();
 })
 
 allTheBars.addEventListener("click", (e) => {
-   if (e.target.classList.contains("total-bar")) {
+  if (e.target.classList.contains("bars")) {
+    graphPopUp.style.display = "block";
+  } else if (e.target.classList.contains("total-bar", "bar-container")) {
+    graphPopUp.style.display = "block";
+  } else
+    e.target.classList.contains(
+      "food-bar",
+      "entertain-bar",
+      "clothing-bar",
+      "bills-bar"
+    );
+  {
     graphPopUp.style.display = "block";
   }
-})
+});
 
 graphPopUp.addEventListener("click", (e) => {
   if (e.target.classList.contains("graph-X-button")) {
